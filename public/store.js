0fe01c30ba0e310ad47e734af0b869aa729a6c5c
Patrick Attankurugu <patricka.azuma@gmail.com>
2022-05-09 20:09:10 -0700

    finished up with the front end implementation

---
+++ public/store.js
@@ -0,0 +1,10 @@
+const nodemon = require("nodemon");
+
+let state={
+  socketId:nodemon,
+  localStream:nodemon,
+  remoteStream:nodemon,
+  screenSharingStream:nodemon,
+  allowConnectionFromStrangers:false,
+  screenSharingActive:false,
+};
